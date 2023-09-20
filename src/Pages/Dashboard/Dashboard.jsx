b6e706a634d4108ad201c474f8e59b2c45ee80fa
CautiousCoder <cautious.coder0@gmail.com>
2023-09-20 12:23:23 +0600

    Portfolio degined.

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,146 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Heading,
+  Icon,
+  Stack,
+  Tag,
+  Text,
+} from "@chakra-ui/react";
+import { BiSolidArrowToBottom, BiSolidArrowToTop } from "react-icons/bi";
+import { BsInfoCircle } from "react-icons/bs";
+import { TbCurrencyTaka } from "react-icons/tb";
 import DashboardLayout from "../../Components/DashboardLayout";
 
 const Dashboard = ({ props }) => {
   return (
     <DashboardLayout title="Dashboard">
-      <div>I am Correct!</div>
+      <Box
+        minH={{
+          base: "calc(100vh - 84px)",
+          md: "calc(100vh - 104px)",
+        }}
+      >
+        <Container
+          maxW="container.lg"
+          bg="white"
+          rounded="lg"
+          py="3"
+          px={{
+            base: "2",
+            md: "4",
+            lg: "6",
+          }}
+        >
+          <Flex justify="space-between">
+            <Stack>
+              <HStack>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="semibold"
+                  fontSize={{
+                    base: "12px",
+                    md: "14px",
+                    lg: "16px",
+                  }}
+                >
+                  total portfolio value
+                </Text>
+                <Icon fontWeight="bold" as={BsInfoCircle} />
+              </HStack>
+              <HStack>
+                <Icon
+                  mt="1"
+                  mr="-3"
+                  fontSize={{
+                    base: "3xl",
+                    md: "4xl",
+                  }}
+                  as={TbCurrencyTaka}
+                />
+                <Heading
+                  as="h1"
+                  fontSize={{
+                    base: "2xl",
+                    md: "3xl",
+                  }}
+                >
+                  112,312.24
+                </Heading>
+              </HStack>
+            </Stack>
+            <Stack>
+              <Text
+                textTransform="capitalize"
+                fontWeight="semibold"
+                fontSize={{
+                  base: "12px",
+                  md: "14px",
+                  lg: "16px",
+                }}
+              >
+                wallet balances
+              </Text>
+              <Flex gap={4}>
+                <HStack>
+                  <Heading
+                    as="h1"
+                    fontSize={{
+                      base: "2xl",
+                      md: "3xl",
+                    }}
+                  >
+                    22.39401000
+                  </Heading>
+                  <Tag>btc</Tag>
+                </HStack>
+                <HStack>
+                  <Icon
+                    mt="1"
+                    mr="-3"
+                    fontSize={{
+                      base: "3xl",
+                      md: "4xl",
+                    }}
+                    as={TbCurrencyTaka}
+                  />
+                  <Heading
+                    as="h1"
+                    fontSize={{
+                      base: "2xl",
+                      md: "3xl",
+                    }}
+                  >
+                    1,300.00
+                  </Heading>
+                  <Tag>btc</Tag>
+                </HStack>
+              </Flex>
+            </Stack>
+            <Stack spacing="4" direction="row" align="center">
+              <Button
+                leftIcon={<Icon as={BiSolidArrowToBottom} />}
+                textTransform="capitalize"
+                colorScheme="teal"
+                size="md"
+              >
+                diposit
+              </Button>
+              <Button
+                leftIcon={<Icon as={BiSolidArrowToTop} />}
+                textTransform="-moz-initial"
+                colorScheme="teal"
+                size="md"
+              >
+                withdraw
+              </Button>
+            </Stack>
+          </Flex>
+        </Container>
+      </Box>
     </DashboardLayout>
   );
 };
